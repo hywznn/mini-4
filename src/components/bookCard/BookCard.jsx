14b581d03408720cfa3b5aa487929137bb49a6c6
hywznn <choihj0510@naver.com>
2026-05-26 11:55:22 +0900

    260526 > Components > bookCard : 도서 목록 북카드 컴포넌트 제작 및 도서목록 페이지 그리드 설정(하드코딩 3)

--- src/components/bookCard/BookCard.jsx
+++ src/components/bookCard/BookCard.jsx
@@ -3,8 +3,9 @@ import "./BookCardStyle.css";
 
 function BookCard({ book, onClick }) {
   return (
-    <article className="book-card" onClick={() => onClick(book.id)}>
+    <article className="book-card" onClick={() => onClick?.(book.id)}>
       <BookImage src={book.coverImageUrl} alt={book.title} />
+
       <div className="book-card-content">
         <h3 className="book-card-title">{book.title}</h3>
         <p className="book-card-desc">{book.content}</p>
--- src/ui/BookListPage.jsx
+++ src/ui/BookListPage.jsx
@@ -1,6 +1,37 @@
 // 도서 목록 페이지
+import BookCard from "../components/bookCard/BookCard";
 
 function BookListPage({ onGoRegister, onGoDetail }) {
+  const sampleBooks = [
+    {
+      id: 1,
+      title: "제목어쩌구",
+      content:
+        "프론트엔드에서 도서 목록을 조회하고, 카드로 출력하며, 상세 페이지로 이동을 실습하기 위한 예제 데이터입니다.",
+      coverImageUrl: "",
+    },
+    {
+      id: 2,
+      title: "제목어쩌구",
+      content:
+        "와 React Router를 활용하여 여러 페이지로 구성된 도서 관리 애플리케이션을 구현하는 예제입니다.",
+      coverImageUrl: "",
+    },
+    {
+      id: 3,
+      title: "제목어쩌구",
+      content:
+        "에이블스쿨 기의 수강생이 이전의 과정에서 실습자료를 혼합한 상록수의 내용을 표지에 담고 있어요.",
+      coverImageUrl: "",
+    },
+    {
+      id: 4,
+      title: "셜록홈즈",
+      content:
+        "괴짜 탐정 셜록 홈즈와 왓슨 박사가 런던의 사건들을 논리적 추리로 해결하는 추리 소설입니다.",
+      coverImageUrl: "",
+    },
+  ];
   return (
     <div>
       {/* 공통 헤더 */}
@@ -14,14 +45,26 @@ function BookListPage({ onGoRegister, onGoDetail }) {
         </nav>
       </header>
 
-      <main>
-        {/* 도서 카드 목록 영역 */}
-        <section>
-          <p>도서 카드 목록 영역 (3열 그리드)</p>
-          {/* 상세페이지 이동(임시로 1번) */}
-          <button type="button" onClick={() => onGoDetail(1)}>
-            샘플 도서 카드 클릭 (id: 1)
-          </button>
+      <main
+        style={{
+          padding: "46px 0",
+
+          backgroundColor: "#f7f6ef",
+        }}
+      >
+        {/* 하드 코딩 되어있습니다 ! */}
+        <section
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(3, 206px)",
+            gap: "36px 42px",
+            justifyContent: "center",
+            alignItems: "start",
+          }}
+        >
+          {sampleBooks.map((book) => (
+            <BookCard key={book.id} book={book} onClick={onGoDetail} />
+          ))}
         </section>
       </main>
     </div>
